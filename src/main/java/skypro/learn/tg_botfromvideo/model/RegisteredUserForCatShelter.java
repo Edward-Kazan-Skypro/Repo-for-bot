54dd4a55b2b906983deb437c654a14e50ec8c58d
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-02-25 12:38:37 +0300

    - добавил классы моделей зарегистрированных пользователей для обоих приютов - добавил соответствующие репозитории

---
+++ src/main/java/skypro/learn/tg_botfromvideo/model/RegisteredUserForCatShelter.java
@@ -0,0 +1,21 @@
+package skypro.learn.tg_botfromvideo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "catShelterUsers")
+@Data
+@NoArgsConstructor
+public class RegisteredUserForCatShelter {
+
+    @Id
+    private Long chatId;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String e_mail;
+}
